fd1da1769701d98d7ea670877b3b279bd84c094f
Alsey Coleman Miller <alseycmiller@gmail.com>
2019-09-10 20:08:37 -0500

    Created package

---
+++ Tests/CloudKitCodableTests/CloudKitCodableTests.swift
@@ -0,0 +1,15 @@
+import XCTest
+@testable import CloudKitCodable
+
+final class CloudKitCodableTests: XCTestCase {
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct
+        // results.
+        XCTAssertEqual(CloudKitCodable().text, "Hello, World!")
+    }
+
+    static var allTests = [
+        ("testExample", testExample),
+    ]
+}
---
+++ Tests/LinuxMain.swift
@@ -0,0 +1,7 @@
+import XCTest
+
+import CloudKitCodableTests
+
+var tests = [XCTestCaseEntry]()
+tests += CloudKitCodableTests.allTests()
+XCTMain(tests)
